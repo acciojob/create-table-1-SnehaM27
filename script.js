85945bfc52adc253150d2132fffc69b5fd987046
SnehaM27 <snehamohan0227@gmail.com>
2025-12-31 17:28:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    const table = document.getElementById("sampleTable");
+
+    // insert row at the top
+    const row = table.insertRow(0);
+
+    // create cells
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    // set values
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
